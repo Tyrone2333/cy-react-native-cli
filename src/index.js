d23c8fd317b262e397ab26fe07396b89bb91555d
enzo <424021424@qq.com>
2020-03-19 10:37:11 +0800

    feat: 优化 package.json 获取

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ require('./utils/checkUpdate')
 module.exports = async function() {
     // cwd是指当前node命令执行时所在的文件夹目录
     // __dirname是指被执行js文件所在的文件夹目录
-    const packageJson = await fs.readJson(path.join(__dirname, '../package.json'))
+    const packageJson = require(path.join(__dirname, '../package.json'))
 
     console.log(`
  ██████╗██╗   ██╗      ██████╗██╗     ██╗
@@ -276,7 +276,7 @@ module.exports = async function() {
                 for (const depName in dependencies) {
                     const version = dependencies[depName].replace(/\^/, '')
                     if (!utils.isItInstalled(depName, rnPkg)) {
-                        // 为防止出事,版本号都固定
+                        // 理论上应该使用 ^向上兼容版本,为防止出事,版本号都固定
                         installDependenciesCmd += ` ${ depName }@${ version } `
                     }
                 }
